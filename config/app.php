6f91a0a003f5ca71f9f875769aaab96b9bab1a77
Alejandro reyes <jorgerenteriareyes4@gmail.com>
2026-06-23 18:53:22 -0600

    fix: se arreglaron soft delete por permisos

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Resend\Laravel\ResendServiceProvider::class,
     ])->toArray(),
 
     /*
