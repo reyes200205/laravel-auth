017d70751961986cf5a26b1391cdf773cf969402
reyes200205 <jorgerenteriareyes4@gmail.com>
2026-06-26 15:54:22 -0600

    FEATURE: se agregaron correciones para poder crear usuarios

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,90 +15,105 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Crear Oficinas
-        $officeUtt = Office::create([
-            'name' => 'UTT',
-            'latitude' => 25.53060477270776,
-            'longitude' => -103.32148524907628,
-            'radius' => 1200, // Aumentado a 1200 metros para permitir el acceso
-            'allowed_ips' => '127.0.0.1',
-        ]);
+        // Crear u obtener Oficinas
+        $officeUtt = Office::updateOrCreate(
+            ['name' => 'UTT'],
+            [
+                'latitude' => 25.53060477270776,
+                'longitude' => -103.32148524907628,
+                'radius' => 1200,
+                'allowed_ips' => '127.0.0.1',
+            ]
+        );
 
-        $officeMasterDrilling = Office::create([
-            'name' => 'Master Drilling',
-            'latitude' => 25.6044566,
-            'longitude' => -103.3870974,
-            'radius' => 1000,
-            'allowed_ips' => '127.0.0.1',
-        ]);
+        $officeMasterDrilling = Office::updateOrCreate(
+            ['name' => 'Master Drilling'],
+            [
+                'latitude' => 25.6044566,
+                'longitude' => -103.3870974,
+                'radius' => 1000,
+                'allowed_ips' => '127.0.0.1',
+            ]
+        );
 
-        $officeCasa = Office::create([
-            'name' => 'Casa',
-            'latitude' => 25.6005072,
-            'longitude' => -103.4151497,
-            'radius' => 500, // 250 metros de margen
-            'allowed_ips' => '127.0.0.1',
-        ]);
-
-
-
+        $officeCasa = Office::updateOrCreate(
+            ['name' => 'Casa'],
+            [
+                'latitude' => 25.6005072,
+                'longitude' => -103.4151497,
+                'radius' => 500,
+                'allowed_ips' => '127.0.0.1',
+            ]
+        );
 
         //===========================================================//
         //usuarios UTT
 
         // Crear Usuarios Administradores (Super Admins)
-        $uttAdmin = User::create([
-            'name' => 'Alessandro (UTT)',
-            'email' => 'sifuentesmarcelo78@gmail.com',
-            'password' => Hash::make('Miri_ta?'),
-            'office_id' => $officeUtt->id,
-        ]);
-        
-        $uttAdmin->assignRole('super-admin');
-
-
+        $uttAdmin = User::updateOrCreate(
+            ['email' => 'sifuentesmarcelo78@gmail.com'],
+            [
+                'name' => 'Alessandro (UTT)',
+                'password' => Hash::make('Miri_ta?'),
+                'office_id' => $officeUtt->id,
+            ]
+        );
+        if (!$uttAdmin->hasRole('super-admin')) {
+            $uttAdmin->assignRole('super-admin');
+        }
 
         //=================================================================//
         // usuarios master
-        $adminMaster = User::create([
-            'name' => 'alejandro (Master Drilling)',
-            'email' => 'renteriareyesjorgealejandro4@gmail.com',
-            'password' => Hash::make('Reyes221119?'),
-            'office_id' => $officeMasterDrilling->id,
-        ]);
-
-        $adminMaster->assignRole('super-admin');
-
+        $adminMaster = User::updateOrCreate(
+            ['email' => 'renteriareyesjorgealejandro4@gmail.com'],
+            [
+                'name' => 'alejandro (Master Drilling)',
+                'password' => Hash::make('Reyes221119?'),
+                'office_id' => $officeMasterDrilling->id,
+            ]
+        );
+        if (!$adminMaster->hasRole('super-admin')) {
+            $adminMaster->assignRole('super-admin');
+        }
 
         // ============================================================//
 
         // ==========================================================================// 
         // usuarios casa 
-        $homeAdmin = User::create([
-            'name' => 'Alejandro (CASA)',
-            'email' => 'reyes221119@gmail.com',
-            'password' => Hash::make('Reyes221119?'),
-            'office_id' => $officeCasa->id,
-        ]);
-        $homeAdmin->assignRole('super-admin');
+        $homeAdmin = User::updateOrCreate(
+            ['email' => 'reyes221119@gmail.com'],
+            [
+                'name' => 'Alejandro (CASA)',
+                'password' => Hash::make('Reyes221119?'),
+                'office_id' => $officeCasa->id,
+            ]
+        );
+        if (!$homeAdmin->hasRole('super-admin')) {
+            $homeAdmin->assignRole('super-admin');
+        }
 
+        $NormalUserCasa = User::updateOrCreate(
+            ['email' => 'jorgerenteriareyes4@gmail.com'],
+            [
+                'name' => 'Alejandro',
+                'office_id' => $officeCasa->id,
+                'password' => Hash::make('Reyes221119?'),
+            ]
+        );
+        if (!$NormalUserCasa->hasRole('user')) {
+            $NormalUserCasa->assignRole('user');
+        }
 
-
-        $NormalUserCasa = User::create([
-            'name' => 'Alejandro',
-            'email' => 'jorgerenteriareyes4@gmail.com',
-            'office_id' => $officeCasa->id,
-            'password' => Hash::make('Reyes221119?'),
-        ]);
-        $NormalUserCasa->assignRole('user');
-
-        $NormalUserUtt = User::create([
-            'name' => 'Igmar Salazar',
-            'email' => 'alereyes221119@gmail.com',
-            'password' => Hash::make('Reyes221119?'),
-            'office_id' => $officeUtt->id
-        ]);
-
-        $NormalUserUtt->assignRole('user');
+        $NormalUserUtt = User::updateOrCreate(
+            ['email' => 'alereyes221119@gmail.com'],
+            [
+                'name' => 'Igmar Salazar',
+                'password' => Hash::make('Reyes221119?'),
+                'office_id' => $officeUtt->id
+            ]
+        );
+        if (!$NormalUserUtt->hasRole('user')) {
+            $NormalUserUtt->assignRole('user');
+        }
     }
 }
